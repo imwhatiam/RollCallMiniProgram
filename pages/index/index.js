e347fe310a318b4275edee75fbc3f83acffd56d7
lian <imwhatiam123@gmail.com>
2025-11-07 17:29:13 +0800

    support offline

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,67 +1,18 @@
-const defaultAvatarUrl = 'https://www.lian-yolo.com/media/images/get-weixin-avatar.jpg'
-import { API } from '../../config';
 Page({
   data: {
-    avatarUrl: defaultAvatarUrl,
-    weixinID: '',
-    hasUserInfo: false,
   },
-
   onLoad() {
-    const cachedWeixinID = wx.getStorageSync('weixinID');
-    if (cachedWeixinID) {
-      this.setData({
-        weixinID: cachedWeixinID,
-        hasUserInfo: true
-      });
-      this.navigateToActivityList();
-    }
   },
-
-  onChooseAvatar(e) {
-    const avatarFilePath = e.detail.avatarUrl;
-    wx.login({
-      success: (res) => {
-        if (res.code) {
-          this.saveUserInfoToServer(res.code, avatarFilePath);
-        } else {
-          console.log('no code from wx.login', res);
-        }
-      },
-      fail: (err) => {
-        console.log('wx.login failed', err);
-      }
-    });
-  },
-
-  navigateToActivityList() {
-    console.log('nav to activity list')
+  navigateToLogin() {
+    console.log('nav to login')
     wx.navigateTo({
-      url: `/pages/activity_list/activity_list`
+      url: `/pages/login/login`
     })
   },
-
-  saveUserInfoToServer(jscode, avatarFilePath) {
-    wx.uploadFile({
-      url: API.saveWeixinUserInfo,
-      filePath: avatarFilePath,
-      name: 'avatar',
-      formData: {
-        code: jscode,
-      },
-      success: (res) => {
-        const data = typeof res.data === 'string' ? JSON.parse(res.data) : res.data;
-        console.log('上传成功', data);
-        wx.setStorageSync('weixinID', data.weixin_id);
-        this.setData({
-          weixinID: data.weixin_id,
-          hasUserInfo: true
-        })
-        this.navigateToActivityList();
-      },
-      fail(err) {
-        console.error('上传失败', err);
-      }
-    });
+  navigateToActivityList() {
+    console.log('nav to offline activity list')
+    wx.navigateTo({
+      url: `/pages/offline/activity_list/activity_list`
+    })
   },
 })
---
+++ pages/login/login.js
@@ -0,0 +1,65 @@
+import { API } from '../../config';
+Page({
+  data: {
+    weixinID: '',
+    hasUserInfo: false,
+  },
+
+  onLoad() {
+    const cachedWeixinID = wx.getStorageSync('weixinID');
+    if (cachedWeixinID) {
+      this.setData({
+        weixinID: cachedWeixinID,
+        hasUserInfo: true
+      });
+      this.navigateToActivityList();
+    }
+  },
+
+  onChooseAvatar(e) {
+    const avatarFilePath = e.detail.avatarUrl;
+    wx.login({
+      success: (res) => {
+        if (res.code) {
+          this.saveUserInfoToServer(res.code, avatarFilePath);
+        } else {
+          console.log('no code from wx.login', res);
+        }
+      },
+      fail: (err) => {
+        console.log('wx.login failed', err);
+      }
+    });
+  },
+
+  navigateToActivityList() {
+    console.log('nav to activity list')
+    wx.navigateTo({
+      url: `/pages/activity_list/activity_list`
+    })
+  },
+
+  saveUserInfoToServer(jscode, avatarFilePath) {
+    wx.uploadFile({
+      url: API.saveWeixinUserInfo,
+      filePath: avatarFilePath,
+      name: 'avatar',
+      formData: {
+        code: jscode,
+      },
+      success: (res) => {
+        const data = typeof res.data === 'string' ? JSON.parse(res.data) : res.data;
+        console.log('上传成功', data);
+        wx.setStorageSync('weixinID', data.weixin_id);
+        this.setData({
+          weixinID: data.weixin_id,
+          hasUserInfo: true
+        })
+        this.navigateToActivityList();
+      },
+      fail(err) {
+        console.error('上传失败', err);
+      }
+    });
+  },
+})
